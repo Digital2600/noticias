53b9c460eef78023e47c6ce8530df36922828964
AlexGonzalez <101224339+Digital2600@users.noreply.github.com>
2024-03-19 12:17:13 -0300

    Add files via upload
    subida 19/03/2024 12:17 pm

---
+++ js/index2.js
@@ -0,0 +1,8 @@
+console.log("Hola Genio Ale");
+console.info("Ale Genio");
+document.addEventListener('DOMContentLoaded', function()  {
+    const fechaYHoraElemento = document.getElementById('fechaYHora');
+    const fechaYHoraActual = new Date();
+    fechaYHoraElemento.textContent = `${fechaYHoraActual}`;
+}); 
+document.html
